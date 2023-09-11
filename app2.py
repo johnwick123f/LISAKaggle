c683d37cc8af1d19e5df56a382a0d6add36b9fbc
johnwick123f <128541091+johnwick123f@users.noreply.github.com>
2023-09-11 11:18:23 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -117,9 +117,9 @@ tokenizer.pad_token = tokenizer.unk_token
 args.seg_token_idx = tokenizer("[SEG]", add_special_tokens=False).input_ids[0]
 
 model = LISAForCausalLM.from_pretrained(
-    "xinlai/LISA-13B-llama2-v1-explanatory", low_cpu_mem_usage=True, use_cache=True, offload_folder="offload", offload_state_dict=True, seg_token_idx=args.seg_token_idx, **kwargs
+    "/kaggle/working/LISAMODEL", low_cpu_mem_usage=True, use_cache=True, offload_folder="offload", offload_state_dict=True, seg_token_idx=args.seg_token_idx, **kwargs
 )
-model.save_pretrained("/kaggle/working/LISAMODEL", max_shard_size="2000MB")
+#model.save_pretrained("/kaggle/working/LISAMODEL", max_shard_size="2000MB")
 model.config.eos_token_id = tokenizer.eos_token_id
 model.config.bos_token_id = tokenizer.bos_token_id
 model.config.pad_token_id = tokenizer.pad_token_id
